2784c94c5b29a76f432a423d2ba3cd921fba5864
yaraoprysk <57788210+yaraoprysk@users.noreply.github.com>
2021-05-31 11:57:09 +0300

    delete.

--- db-lab-6/src/main/java/com/lab6/controller/implementation/HealthController.java
+++
@@ -1,9 +0,0 @@
-package com.lab6.controller.implementation;
-
-@RestController
-public class HealthController {
-    @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
-    public ResponseEntity returnHealthCheckResponse() {
-        return new ResponseEntity<>("Everything is OK", HttpStatus.OK);
-    }
-}
